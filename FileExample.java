99fab28b6ff34fe4f90191543992999214a05908
Fulliar <67739124+Fulliar@users.noreply.github.com>
2022-12-05 14:04:47 +0300

    Add files via upload

---
+++ FileExample.java
@@ -0,0 +1,88 @@
+package com.bilgeadam.hw.file;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class FileExample {
+
+	public static void main(String[] args) {
+		
+		
+		String dosyaYolu = "C:\\Users\\erguv\\OneDrive\\Desktop\\Bilge Adam Boost\\File_Example\\deneme.txt";
+		File file = new File(dosyaYolu);
+		try {
+			dosyaYarat(file);
+		}catch (Exception ex) {
+			System.out.println("Dosya oluşturulurken hata oluştu");
+		}
+		
+		try {
+			dosyaYaz(file);
+		} catch (FileNotFoundException e) {
+			System.out.println("Dosya bulunamadı");
+		} catch(Exception ex) {
+			System.out.println("Genel hata");
+		}
+		
+		try {
+			dosyaOku(file);
+		} catch (IOException e) {
+			System.out.println("Dosya okuma hatası");
+		}	
+	}
+	
+	public static void dosyaYarat(File file) throws Exception
+	{
+			if(file.exists()) {
+				
+				System.out.println("Böyle bir dosya zaten var.");
+								
+			}else {
+				if(!file.createNewFile()) {
+					System.out.println("Dosya oluşturulurken hata oluştu");
+				} else {
+					System.out.println("Dosya oluşturuldu");
+				}
+			}
+		 	}
+	
+	public static void dosyaYaz(File file) throws FileNotFoundException, Exception
+	{
+			if(file.exists()) 
+			{
+				Scanner scanner = new Scanner(System.in);
+				FileWriter fileWriter = new FileWriter(file);
+				BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+				System.out.println("Dosyaya yazılacaklar:");
+				bufferedWriter.write(scanner.nextLine());
+				bufferedWriter.close();
+				scanner.close();
+				System.out.println("Dosya yazma işlemi başarılı");	
+			} else {
+				System.out.println("Dosya bulunamdi.");
+			}	
+	}
+	
+	public static void dosyaOku(File file) throws IOException
+	{
+		if(file.exists()) 
+		{
+			String line;
+			FileReader fileReader = new FileReader(file);
+			BufferedReader bufferedReader = new BufferedReader(fileReader);
+			
+			while((line = bufferedReader.readLine()) != null) {
+				System.out.println(file.getName() + " isimli dosyadan okunanlar : \n" + line);
+			}
+			
+		}else {
+			System.out.println("Dosya bulunamdi.");
+		}	
+	}
+}
